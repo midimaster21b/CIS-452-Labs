be2c6252ebefcd978f0b1131e85f7bc52f364158
Joshua Edgcombe <joshedgcombe@gmail.com>
2018-09-12 15:57:18 -0400

    Allow specification of who the usage statistics will be for

--- shell.c
+++ shell.c
@@ -10,18 +10,44 @@
 #include <stdio.h>
 #include <sys/resource.h>
 #include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <wait.h>
 
-void print_proc_stats();
+void print_proc_stats(int proc_who);
 
 int main() {
+
+  int proc_retval;
+  pid_t proc_pid;
+
+  pid_t child_pid = fork();
+
+  // Child code
+  if(child_pid == 0) {
+    execv("ls", NULL);
+  }
+
+  // Parent code
+  else if(child_pid > 0) {
+    proc_pid = wait(&proc_retval);
+
+    /* print_proc_status(RUSAGE_SELF); */
+    print_proc_stats(RUSAGE_CHILDREN);
+  }
+
+  // Error code (anything negative)
+  else {
+    printf("Error: An error occurred when forking the command.\n");
+  }
 
   return(0);
 }
 
-void print_proc_stats() {
+void print_proc_stats(int proc_who) {
   // Initialize variables for resource usage statistics
   int usage_retval;
-  int process_who = RUSAGE_SELF;
+  int process_who = proc_who;
   struct rusage process_usage;
   time_t process_user_seconds;
   long   process_user_microseconds;
