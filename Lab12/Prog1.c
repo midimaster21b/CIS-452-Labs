8b4cd0caef311aae3a2b9226506dc05f002ba08c
Joshua Edgcombe <joshedgcombe@gmail.com>
2018-11-29 10:34:12 -0500

    Working Prog1.c

---
+++ Lab12/Prog1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <errno.h>
+
+int main(int argc, char *argv[]) {
+  struct stat statBuf;
+
+  if(argc < 2) {
+    printf("Usage: filename required\n");
+    exit(1);
+  }
+
+  if(stat(argv[1], &statBuf) < 0) {
+    perror("huh?  there is ");
+    exit(1);
+  }
+
+  if((S_IFREG & statBuf.st_mode) > 0) {
+    printf("Regular file\n");
+  }
+  else if((S_IFDIR & statBuf.st_mode) > 0) {
+    printf("Directory\n");
+  }
+
+  /* printf("value is: %u\n", statBuf.st_mode);  */
+  return 0;
+}
