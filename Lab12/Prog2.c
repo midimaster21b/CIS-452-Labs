44b1ac75dd2514a8645787630d2cb2ff92b28d90
Joshua Edgcombe <joshedgcombe@gmail.com>
2018-11-29 11:03:52 -0500

    Finish Prog2

--- Lab12/Prog2.c
+++ Lab12/Prog2.c
@@ -8,11 +8,19 @@ int main()
 {
   DIR *dirPtr;
   struct dirent *entryPtr;
+  struct stat statBuf;
 
   dirPtr = opendir(".");
 
-  while((entryPtr = readdir(dirPtr)))
-    printf("%-20s\n", entryPtr->d_name);
+  while((entryPtr = readdir(dirPtr))) {
+    printf("%-20s", entryPtr->d_name);
+
+    // Fill the stat struct
+    stat(entryPtr->d_name, &statBuf);
+
+    printf(" \t %ld bytes\n", statBuf.st_size);
+  }
+
   closedir(dirPtr);
 
   return 0;
