e2a092d1c3bd6dbfe197890e222d041ea115cc6f
Joshua Edgcombe <joshedgcombe@gmail.com>
2018-12-06 10:53:04 -0500

    Initial Prog1

---
+++ Lab14/Prog1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  char name[32];
+  char command[64];
+
+  printf("Enter your eos username: ");
+  scanf("%s", name);
+  sprintf(command, "groups %s", name);
+  system(command);
+  return 0;
+}
